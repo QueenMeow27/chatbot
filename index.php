550ddf2d628b57522068077ec453086aaabf6ded
QueenMeow27 <44196044+QueenMeow27@users.noreply.github.com>
2018-11-14 18:47:11 +1100

    Update index.php

--- index.php
+++ index.php
@@ -4,37 +4,42 @@
 </HEAD>
 <BODY BGCOLOR="PINK">
 <CNETER> WELCOME </CENTER>
-  
 <?php
-echo "hi"
+
 $method = $_SERVER['REQUEST_METHOD'];
+
 //process only when method id post
+
 if($method =="POST"){
+
 $requestBody = file_get_contents('php://input');
 $json = json_decode($requestBody);
-$text = $json->result>parameters->text;
+$text = $json->result->parameters->text;
+
 switch($test) {
-case 'bindhiya':
-$speech = "Hi Bindhiya.. Nice to meet you";
-break;
-case 'ramya':
-$speech = "Hi Ramya.. Nice to meet you";
-break;
-default:
-$speech = "Hello All..";
-break;
+	case 'bindhiya':
+		$speech = "Hi Bindhiya.. Nice to meet you";
+		break;
+	case 'ramya':
+		$speech = "Hi Ramya.. Nice to meet you";
+		break;
+	default:
+		$speech = "Hello All..";
+		break;
 }
-$response = new stdClass();
+
+$response = new \stdClass();
 $response->speech="";
 $response->displayText="";
 $response->source= "webhook";
-echo json_encode($response);
+
 }
 else
 {
-echo "Method not allowed";
+	echo "Method not allowed";
 }
+
 ?>
-  
+
 </BODY>
-  </HTML>
+</HTML>
