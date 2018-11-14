c7f696bea01cf9734ba495602cc9a8906edeb4a5
QueenMeow27 <44196044+QueenMeow27@users.noreply.github.com>
2018-11-14 19:28:10 +1100

    Update index.php

--- index.php
+++ index.php
@@ -10,31 +10,26 @@ if($method =="POST"){
 $requestBody = file_get_contents('php://input');
 $json = json_decode($requestBody);
 $text = $json->queryResult->queryText;
-
-echo "Inside post method";
-	
-echo $requestBody;
-echo $text;
 	
 switch($text) {
 	case 'bindhiya':
-		$speech = "Hi Bindhiya.. Nice to meet you";
+		$fulfillmentText = "Hi Bindhiya.. Nice to meet you";
 		break;
 	case 'ramya':
-		$speech = "Hi Ramya.. Nice to meet you";
+		$fulfillmentText = "Hi Ramya.. Nice to meet you";
 		break;
 	default:
-		$speech = "Hello All..";
+		$fulfillmentText = "Hello All..";
 		break;
 }
-echo $speech;
+echo $fulfillmentText;
 $response = new \stdClass();
-$response->speech="default response";
-$response->displayText="xxxxxxxxxxxxxxxxxxx";
+$response->fulfillmentText=$fulfillmentText;
+$response->displayText="";
 $response->source= "webhook";
 
 echo $response;
-echo $response->speech;
+echo $response->fulfillmentText;
 }
 else
 {
