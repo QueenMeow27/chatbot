9e7fded0d2957cd22eb4ba20a80e24c6ee687349
QueenMeow27 <44196044+QueenMeow27@users.noreply.github.com>
2018-11-15 00:04:43 +1100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ if($method =="POST"){
 $requestBody = file_get_contents('php://input');
 $json = json_decode($requestBody);
 $text = $json->queryResult->queryText;
-	
+echo $json;
+echo $text;
 switch($text) {
 	case 'bindhiya':
 		$fulfillmentText = "Hi Bindhiya.. Nice to meet you";
@@ -29,8 +30,8 @@ $response->displayText="This is the display text";
 $response->source= "webhook";
 
 echo $response;
-	echo $response->displayText;
-	echo $response->source;
+echo $response->displayText;
+echo $response->source;
 
 	$res = json_encode($response);
 	echo $res;
