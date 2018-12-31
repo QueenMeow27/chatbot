ffda5c3b8e5792f88a17f5ba58778ba17448cb43
QueenMeow27 <44196044+QueenMeow27@users.noreply.github.com>
2018-12-31 17:52:24 +1100

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,17 @@ if($method =="POST"){
 $requestBody = file_get_contents('php://input');
 $json = json_decode($requestBody);
 $text1 = $json->queryResult->queryText;
-$ans = $json->queryResult->fulfillmentText;	
+$id = $json->queryResult->fulfillmentText;	
 
 	
 
 	
 switch($text1) {
-	case 'give me yes no picklist':
+	case 'picklist':
+		header('Content-Type: application/json');	
+		$appres = file_get_contents('https://my.wowcareers.com.au/myprofile2api_basic/wow/candidate/Picklist2?picklistId=yesNo');	
+		$newdata = json_encode($appres);
+	        echo $newdata;
 		$fulfillmentText = "Hi Bindhiya";
 		break;
 	case 'picklist':
@@ -38,11 +42,11 @@ $text->text1 = $text1->text2;
 $queryResult->fulfillmentMessages = $text->text1;
 $queryResult->source= "webhook";
 
-header('Content-Type: application/json');	
-echo json_encode($queryResult);
-$appres = file_get_contents('https://my.wowcareers.com.au/myprofile2api_basic/wow/candidate/Picklist2?picklistId=yesNo');	
-$newdata = json_encode($appres);
-echo $newdata;
+//header('Content-Type: application/json');	
+//echo json_encode($queryResult);
+//$appres = file_get_contents('https://my.wowcareers.com.au/myprofile2api_basic/wow/candidate/Picklist2?picklistId=yesNo');	
+//$newdata = json_encode($appres);
+//echo $newdata;
 
 	
 return json_encode($queryResult);
